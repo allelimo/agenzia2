7a3bbbdd680ebbf493fc2498847fe14d4201e1a2
alessandro limonta <allelimo@gmail.com>
2020-04-10 01:53:32 +0200

    pagina autoveicolo e classe per impostazioni

---
+++ agenzia2/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Windows.UI.Popups;
+
+namespace agenzia2
+{
+    public static class GlobalData
+    {
+        // alle inserire qui gli array comuni per le diverse view
+        public static string[] myarray1 = new string[10];
+
+        //? inserito il controllo dell'essitenza del file
+        public static async void LoadDatainArray(string myfile_to_open, Array my_array_to_load)
+        {
+            int i = 0;
+            string mystring = null;
+            //alle accedere al file nello storage di windows
+            try
+            {
+                Windows.Storage.StorageFolder strFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                Windows.Storage.StorageFile strFile = await strFolder.GetFileAsync(myfile_to_open);
+
+                if (strFile != null)
+                {
+                    var buffer = await Windows.Storage.FileIO.ReadBufferAsync(strFile);
+                    using (var dataReader = Windows.Storage.Streams.DataReader.FromBuffer(buffer))
+                    {
+                        //alle scrivi il file in un buffer
+                        mystring += dataReader.ReadString(buffer.Length);
+                    }
+                    //! using System.IO; //per stringreader qui sotto
+                    //alle usare i dati nel buffer per popolare gli array
+                    using (StringReader sr = new StringReader(mystring))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            //alle scrivi ogni linea in un valore dell'array
+                            my_array_to_load.SetValue(line, i);
+                            i++;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageDialog mydlg = new MessageDialog("File di configurazione non presenti 1");
+                await mydlg.ShowAsync();
+            }
+        }
+
+        //alle apri il file txt pee l'editing diretto
+        public static async void OpenSettingFile(string myfile_to_open)
+        {
+            try
+            {
+                Windows.Storage.StorageFolder strFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                Windows.Storage.StorageFile strFile = await strFolder.GetFileAsync(myfile_to_open);
+
+                if (strFile != null)
+                {
+                    //alle apri il dile con la'pplicazione di default
+                    var success = await Windows.System.Launcher.LaunchFileAsync(strFile);
+                }
+            }
+            catch (Exception)
+            {
+                MessageDialog mydlg = new MessageDialog("File di configurazione non presenti 4");
+                await mydlg.ShowAsync();
+            }
+        }
+    }
+}
--- agenzia2/Views/AutoPage.xaml.cs
+++ agenzia2/Views/AutoPage.xaml.cs
@@ -3,6 +3,9 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+
+using System.Linq;
 
 namespace agenzia2.Views
 {
@@ -11,6 +14,16 @@ namespace agenzia2.Views
         public AutoPage()
         {
             InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+
+            // alle qui: caricare i dati negli array
+            GlobalData.LoadDatainArray("art15.txt", GlobalData.myarray1);
+            //! solo test
+            GlobalData.OpenSettingFile("art14.txt");
+
+            // alle selezionare la text box dei kwh
+            // ? non funziona: TxtKwh.Focus(FocusState.Programmatic);
+            TxtKwh.SelectAll();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -27,5 +40,160 @@ namespace agenzia2.Views
         }
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        // alle variabili per radiobutton
+        private string MyRdbScelta = null;
+        // alle variabili per toggleswitch
+        private bool bPRA, bEpoca, bDoppia = false;
+        //alle variabili per ipt con default
+        private double dbPra = 76.60;
+        private double dbMtc = 29.76;
+        private double dbAltre = 6;
+        private double dbCorrispettivi = 118.34;
+        private double db53esente = 0;
+        private double db53impiva = 92.44;
+        private double dbPraDini = 63.1;
+        private double dbNotaPra = 20;
+
+        private int sconto1 = 15;
+        private int sconto2 = 10;
+        private int sconto3 = 5;
+
+        private void RdbGruppo_Checked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            RadioButton rbd = sender as RadioButton;
+            MyRdbScelta = rbd.Name;
+            //! per usare messagedialog serve windows.ui.popups
+            //alle dialogo da cancellare una volta controllato il funzionamento
+            //var mydlg = new MessageDialog(MyRdbScelta + " selected");
+            //await mydlg.ShowAsync();
+            if (MyRdbScelta == "RdbSuccessione")
+            {
+                TswPra.IsOn = true;
+                bPRA = true;
+            }
+            else
+            {
+                TswPra.IsOn = false;
+                bPRA = false;
+
+            }
+        }
+        private void Tsw_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            bPRA = TswPra.IsOn;
+            bEpoca = TswEpoca.IsOn;
+            bDoppia = TswDoppia.IsOn;
+            //! per usare messagedialog serve windows.ui.popups
+            //! aggiungere async
+            //alle dialogo da cancellare una volta controllato il funzionamento
+            //var mydlg = new MessageDialog("pra " + bPRA + " epoca " + bEpoca + " doppia " + bDoppia);
+            //await mydlg.ShowAsync();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (ScegliCaso() == 1)
+                CalcolaIptProporzionale();
+            else
+                CalcolaIptFissa();
+        }
+
+        //alle calcola ipt proporzionale usando i dati degli array
+        private void CalcolaIptProporzionale()
+        {
+            //alle controlla se c'è input, se no annulla la funzione
+            if (!string.IsNullOrEmpty(TxtKwh.Text))
+            {
+                int numKwh = int.Parse(TxtKwh.Text);
+                db53esente = 307.56;
+
+                if (numKwh <54)
+                {
+                    if (bPRA)
+                        db53esente += dbNotaPra;
+
+                    double db53totale = db53esente + db53impiva;
+                    TxtEsente.Text = db53esente.ToString();
+                    TxtImpiva.Text = db53impiva.ToString();
+                    TxtTotale.Text = db53totale.ToString();
+
+                }
+                else
+                {
+                    //arrotonda normale
+                    double dbIpt = Math.Round(numKwh * 4.57);
+
+                    double dbTemptot = dbIpt + dbMtc + dbPra + dbAltre + dbCorrispettivi - sconto1;
+                    //arrotonda al 5 piu vicino
+                    double dbTotale = 5 * (int)Math.Round(dbTemptot / 5);
+                    double dbEsente = dbIpt + dbMtc + dbPra;
+
+                    if (bPRA)
+                    {
+                        dbEsente += dbNotaPra;
+                        dbTotale += dbNotaPra;             
+                    }
+
+                    double dbImpiva = dbTotale - dbEsente;
+
+                    TxtTotale.Text = dbTotale.ToString();
+                    TxtImpiva.Text = dbImpiva.ToString();
+                    TxtEsente.Text = dbEsente.ToString();
+                }
+            }
+        }
+
+        //private void TxtKwh_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        //{
+        //    sender.Text = new String(sender.Text.Where(char.IsDigit)ToArray());
+        //}
+
+        private void TxtKwh_BeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            args.Cancel = args.NewText.Any(c => !char.IsDigit(c));
+        }
+
+        //alle calcola ipt con valori fissi
+        private void CalcolaIptFissa()
+        {
+            string strImpiva;
+            string strEsente;
+            //numKwh = int.Parse(TxtKwh.Text);
+            int myScelta = ScegliCaso();
+
+            strImpiva = GlobalData.myarray1[myScelta];
+            strEsente = GlobalData.myarray1[myScelta];
+
+            TxtImpiva.Text = "strImpiva";
+            TxtEsente.Text = "strEsente";
+
+
+        }
+        //alle controlla bottoni e switch per decidere il tipo di tarsferimento richiesto
+        private int ScegliCaso()
+        {
+            if(MyRdbScelta == "RdbTrasferimento" && !bEpoca)
+                return 1; //!normale
+            if (MyRdbScelta == "RdbTrasferimento" && bEpoca)
+                return 2; //!epoca
+            else if (MyRdbScelta == "RdbSuccessione" && !bEpoca && !bDoppia)
+                return 3; //!successione normale
+            else if (MyRdbScelta == "RdbSuccessione" && bEpoca && !bDoppia)
+                return 4; //!successione epoca
+            else if (MyRdbScelta == "RdbSuccessione" && bDoppia)
+                return 5; //!successione doppia
+            else if (MyRdbScelta == "RdbDini" && !bEpoca)
+                return 6; //!dini nromale
+            else if (MyRdbScelta == "RdbDini" && bEpoca)
+                return 7; //!dini epoca
+            else if (MyRdbScelta == "RdbAtto" && !bEpoca)
+                return 8; //!separazione normale
+            else if (MyRdbScelta == "RdbAtto" && bEpoca)
+                return 9;//!separazione epoca
+            else
+                return 0;
+        }
     }
 }
