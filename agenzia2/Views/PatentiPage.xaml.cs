09a14b63637199b573577e8b0769b705c09fe959
alessandro limonta <allelimo@gmail.com>
2020-04-15 00:04:40 +0200

    inizio pagina patenti

--- agenzia2/Views/PatentiPage.xaml.cs
+++ agenzia2/Views/PatentiPage.xaml.cs
@@ -27,5 +27,58 @@ namespace agenzia2.Views
         }
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        // alle variabili per radiobutton
+        private string MyRdbScelta = null;
+        // alle variabili per toggleswitch
+        private bool bDeterioramento, bAzienda = false;
+
+        private void RdbGruppo_Checked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            RadioButton rbd = sender as RadioButton;
+            MyRdbScelta = rbd.Name;
+
+            try
+            {
+                if (TswDeterioramento == null)
+                    return;
+                else
+                {
+                    TswDeterioramento.IsOn = false;
+                    //TswAzienda.IsOn = false;
+                }
+
+            }
+            catch
+            {
+
+            }
+        }
+        private void Tsw_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            bDeterioramento = TswDeterioramento.IsOn;
+            //bAzienda = TswAzienda.IsOn;
+            //! per usare messagedialog serve windows.ui.popups
+            //! aggiungere async
+            //alle dialogo da cancellare una volta controllato il funzionamento
+            //var mydlg = new MessageDialog("pra " + bPRA + " deterioramento " + bDeterioramento + " crono " + bCrono + " extraue" + bExtraUE);
+            //await mydlg.ShowAsync();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            //ScegliCaso();
+            //var mydlg = new MessageDialog(ScegliCaso().ToString());
+            //await mydlg.ShowAsync();
+           // CalcolaIptFissa();
+
+        }
+
+        private void HyperlinkButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            HyperlinkButton hpl = sender as HyperlinkButton;
+            GlobalData.OpenSettingFile(hpl.Name + ".txt");
+        }
+
     }
 }
