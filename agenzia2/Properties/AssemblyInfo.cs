847623bfdc8e59a52cbb759b7864ad681ddb4758
alessandro limonta <allelimo@gmail.com>
2022-06-07 15:03:41 +0200

    bottone calcola in autopage anche per conversione cv e kwh

--- agenzia2/Properties/AssemblyInfo.cs
+++ agenzia2/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.22157.1040")]
-[assembly: AssemblyFileVersion("1.3.22157.1040")]
+[assembly: AssemblyVersion("1.3.22158.1258")]
+[assembly: AssemblyFileVersion("1.3.22158.1258")]
 [assembly: ComVisible(false)]
