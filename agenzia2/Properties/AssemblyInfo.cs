295121cc29c66b66cb56965d25ffd1ffc8f61c50
alessandro limonta <allelimo@gmail.com>
2022-06-30 10:01:35 +0200

    taglianod veicoli storici

--- agenzia2/Properties/AssemblyInfo.cs
+++ agenzia2/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.22180.1547")]
-[assembly: AssemblyFileVersion("1.4.22180.1547")]
+[assembly: AssemblyVersion("1.4.22181.0759")]
+[assembly: AssemblyFileVersion("1.4.22181.0759")]
 [assembly: ComVisible(false)]
