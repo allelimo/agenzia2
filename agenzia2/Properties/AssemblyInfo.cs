deb39073d60142ecd5fcef59fe7d5e63f1a868e7
alessandro limonta <allelimo@gmail.com>
2022-10-18 15:44:20 +0200

    new release

--- agenzia2/Properties/AssemblyInfo.cs
+++ agenzia2/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.22291.1318")]
-[assembly: AssemblyFileVersion("1.4.22291.1318")]
+[assembly: AssemblyVersion("1.4.22291.1334")]
+[assembly: AssemblyFileVersion("1.4.22291.1334")]
 [assembly: ComVisible(false)]
